049ae69610386a830464029cd9b0d8deff48e9ee
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2022-08-12 14:46:33 +1200

    v0.1.2
    Fix stock price downloading and pre-processing

--- downloadprices.py
+++ downloadprices.py
@@ -7,7 +7,7 @@ parser.add_argument('file_name')
 args = parser.parse_args()
 
 def get_prices(tickers):
-    data= yf.download(tickers, start="2016-01-01", end="2020-01-01")
+    data = yf.download(tickers, start="2016-01-01", end="2020-01-01")
     print("Downloaded prices!")
     data.to_pickle(args.file_name)
 
